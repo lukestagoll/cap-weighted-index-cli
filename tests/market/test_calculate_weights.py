0dcbe847669abdd37ee20437dbb6070bdf54ccb6
Luke <lukestagoll16@gmail.com>
2025-05-18 18:10:38 +1000

    Add: test case for calculating weights with pre-existing weight values

--- tests/market/test_calculate_weights.py
+++ tests/market/test_calculate_weights.py
@@ -5,24 +5,37 @@ import pandas.testing as pdt
 from cap_weighted_index_cli.market.calculate_weights import calculate_weights
 
 class TestCalculateWeights(unittest.TestCase):
-    def test_unsorted_values(self):
+    def test_calculate_weights(self):
         # Arrange
         df = pd.DataFrame({
-            "market_cap_m": [
-                1000,
-                200,
-                100,
-                50,
-            ]
+            "market_cap_m": [1000, 200, 100, 50]
         })
         total_market_cap = 1350
         expected = pd.DataFrame({
-            "market_cap_m": [
-                1000,
-                200,
-                100,
-                50,
-            ],
+            "market_cap_m": [1000, 200, 100, 50],
+            "weight": [
+                np.float64(1000 / 1350),
+                np.float64(200 / 1350),
+                np.float64(100 / 1350),
+                np.float64(50 / 1350),
+            ]
+        })
+
+        # Act
+        actual = calculate_weights(df, total_market_cap)
+
+        # Assert
+        pdt.assert_frame_equal(actual.reset_index(drop=True), expected)
+
+    def test_calculate_weights_with_existing_values(self):
+        # Arrange
+        df = pd.DataFrame({
+            "market_cap_m": [1000, 200, 100, 50],
+            "weight": [0, 0, 0, 0]
+        })
+        total_market_cap = 1350
+        expected = pd.DataFrame({
+            "market_cap_m": [1000, 200, 100, 50],
             "weight": [
                 np.float64(1000 / 1350),
                 np.float64(200 / 1350),
